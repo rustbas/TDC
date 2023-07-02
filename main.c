0e558ac6d8b89bb9929c14d14ee1725124d3f621
rustbas <hrustbas@gmail.com>
2023-07-02 14:14:57 +0300

    implemented currLine (without render)

--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define TODO_NAME_SIZE 100
 #define SIZE_TODO_LIST 2
 
+#define min(x,y) (((x) < (y)) ? (x) : (y))
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+
 bool quit = false;
+int currLine = 0;
 
 void control(char c)
 {
   switch(c)
     {
-      case 'q':
-	quit = true;
-	break;
+    case 'q':
+      quit = true;
+      break;
+    case 'j':
+      currLine = min(currLine + 1, SIZE_TODO_LIST);
+      break;
+    case 'k':
+      currLine = max(currLine - 1, 0);
+      break;
     }
 }
 
@@ -37,11 +48,24 @@ int main(void){
 
   int sizeY, sizeX;
   char msg[] = "Hello, World";
+
+  //Check color support
+  if (has_colors() == false) {
+    endwin();
+    printf("Your terminal does not support color\n");
+    exit(1);
+  }
+
+  start_color();
   
+  init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_BLUE, COLOR_RED);
+
   while (!quit)
     {
       // Get the size of the terminal
       getmaxyx(stdscr, sizeY, sizeX);
+      WINDOW *win = newwin(0, currLine, 1, sizeX-3);
       
       // Clean the screen
       erase();
@@ -49,11 +73,16 @@ int main(void){
       // Render stuff
       //move(sizeY/2, (sizeX-strlen(msg))/
       //printw(msg);    
-
+      bkgd(COLOR_PAIR(1));
+      wbkgd(win, COLOR_PAIR(2));
+      
       for (int i = 0; i < SIZE_TODO_LIST;i++) {
 	move(i, 0);
-	printw(tds[i].name);
+	printw("- [%d]: %s", i+1, tds[i].name);
       }
+
+      move(SIZE_TODO_LIST, 0);
+      printw("currLine = %d", currLine);
       
       // Get the control char
       control(getch());
